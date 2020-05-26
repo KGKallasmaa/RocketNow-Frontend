3b1340f412ae30c11d63d520a4192f645a28fd2f
karl96k <karl.gustav1789@gmail.com>
2020-05-26 20:44:12 +0300

    testing netflify

--- src/pages/checkout/cancel.js
+++ src/pages/checkout/cancel.js
@@ -1,29 +1,29 @@
-import React from "react";
-import {Helmet} from "react-helmet";
-import Footer from "../../components/footer.jsx";
-import {Navbar} from "../../components/navbar.jsx";
-import AcceptsCookies from "../../components/cookieConsent";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Footer from '../../components/footer.jsx';
+import { Navbar } from '../../components/navbar.jsx';
+import AcceptsCookies from '../../components/cookieConsent';
 
 export default class Cancel extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cancel_id: this.props.cancel_id,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      cancel_id: this.props.cancel_id,
+    };
+  }
 
-    render() {
-        return (
-            <div className="container-fluid">
-                <Navbar/>
-                <Helmet>
-                    <title>Your order was canceled on RocketNow</title>
-                </Helmet>
-                <AcceptsCookies/>
-                <h1>This page is still under construction</h1>
-                <p>Cancel page</p>
-                <Footer/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container-fluid">
+        <Navbar />
+        <Helmet>
+          <title>Your order was canceled on RocketNow</title>
+        </Helmet>
+        <AcceptsCookies />
+        <h1>This page is still under construction</h1>
+        <p>Cancel page</p>
+        <Footer />
+      </div>
+    );
+  }
 }
